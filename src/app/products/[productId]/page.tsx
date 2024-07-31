5b0b30e174e01f4c32ffe6019929b17baff29897
Florencia Accastelli <f.accastelli@gmail.com>
2024-07-31 11:47:32 -0300

    routing metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Metadata } from 'next'
 
-export default function ProductDetails({ params }: {
-    params: { productId: string }
-}) {
+type Props = {
+  params: {
+    productId: string
+  }
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`)
+    }, 100)
+  })
+  return {
+    title: `Product ${title}`
+  }
+}
+
+export default function ProductDetails({ params }: Props) {
   return (
     <h1>Details about Product {params.productId}</h1>
   )
