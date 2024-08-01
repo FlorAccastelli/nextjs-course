5064e8777044cc03447e7e249f3cc0e7d9ce51c5
Florencia Accastelli <f.accastelli@gmail.com>
2024-08-01 11:56:25 -0300

    link component navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-   <h1>Welcome home!</h1>
+    <div>
+      <h1>Welcome home!</h1>
+      <Link href={"/blog"}>Blog</Link>
+      <Link href={"/products"}>Products</Link>
+    </div>
+   
   )
 }
