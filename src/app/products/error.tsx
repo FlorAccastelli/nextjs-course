9a838e2fdc9a09d9dd768e950f02e6bccc8bfbf9
Florencia Accastelli <f.accastelli@gmail.com>
2024-08-01 17:41:24 -0300

    handling errors in layouts

---
+++ src/app/products/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react'
+
+export default function ErrorBundary({ error, reset }: {
+    error: Error; reset: () => void;
+}) {
+  return (
+    <div>
+        {error.message}
+        <button onClick={reset}>Try again</button>
+    </div>
+  )
+}
